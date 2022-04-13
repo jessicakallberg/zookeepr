9f9a6bc22a38ba3371f57e48bd9f36bb532f2455
Jessica Santiago <jessie_13084@yahoo.com>
2022-04-13 15:06:58 -0400

    adding to the app.get

--- server.js
+++ server.js
@@ -16,10 +16,30 @@ const { animals } = require('./data/animals.json')
 
 
 
+function filterByQuery(query,animalsArray)  {
+    let filterdResults = animalsArray;
+    if (query.diet) {
+        filterdResults = filterdResults.filter(animal => animal.diet === query.diet);
+    }
 
+    if(query.species) {
+        filterdResults = filterdResults.filter(animal.species === query.species);
+    }
+
+    if (query.name) {
+        filterdResults = filterdResults.filter(animal => animal.name === query.name);
+    }
+
+    return filterdResults;
+}
 
 app.get('/api/animals.json', (req, res) => {
-    res.json('animals.json');
+    let results = animals;
+    if (req.query){
+        results = filterByQuery(req.query, results);
+    }
+
+    res.json(results);
 });
 
 app.listen(3001, () => {
